77fd8ab14bc0843f158adb8516d94e2c54e1b388
Roman Morozov <rmorozov@wio.io>
2024-08-27 12:59:11 +0400

    revert message changes

--- lib/test_widget.dart
+++ lib/test_widget.dart
@@ -2,17 +2,11 @@ import 'package:flutter/material.dart';
 
 class TestWidget extends StatelessWidget {
   final String count;
-  final String message;
 
-  const TestWidget({super.key, required this.count, required this.message});
+  const TestWidget({super.key, required this.count});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(count, style: TextStyle(fontSize: 40)),
-        Text(message, style: TextStyle(fontSize: 40)),
-      ],
-    );
+    return Text(count, style: TextStyle(fontSize: 36));
   }
 }
