6e76b20cca1de3a82bcbe08ffcf27ad1efdd774a
Roman Morozov <rmorozov@wio.io>
2024-08-27 12:55:26 +0400

    add message

--- lib/test_widget.dart
+++ lib/test_widget.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';
 
 class TestWidget extends StatelessWidget {
   final String count;
+  final String message;
 
-  const TestWidget({super.key, required this.count});
+  const TestWidget({super.key, required this.count, required this.message});
 
   @override
   Widget build(BuildContext context) {
-    return Text(count, style: TextStyle(fontSize: 40));
+    return Column(
+      children: [
+        Text(count, style: TextStyle(fontSize: 40)),
+        Text(message, style: TextStyle(fontSize: 40)),
+      ],
+    );
   }
 }
